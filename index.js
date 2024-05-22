5602c42a67b263a8bd686767da2cf0f1952e05bd
InnoSufiyan <innosufiyan@gmail.com>
2024-05-22 21:41:24 +0500

    signup login work done

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ app.get('/', (req, res)=> {p
 app.post('/about', (req, res)=> {
     res.send('hello world')
 })
+app.post('/signup', (req, res)=> {
+    res.send('Signup successfull')
+})
+app.post('/login', (req, res)=> {
+    res.send('Login successfull')
+})
 
 app.listen(4000, ()=> {
     console.log('server chal gaya')
